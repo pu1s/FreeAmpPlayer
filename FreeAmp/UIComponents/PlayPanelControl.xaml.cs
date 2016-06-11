42eeba3b9d69b9b4dd6fc825a8a781659ce724e5
pu1s <pu1s@outlook.com>
2016-06-11 08:40:50 +0300

    112

---
+++ FreeAmp/UIComponents/PlayPanelControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FreeAmp.UIComponents
+{
+    /// <summary>
+    /// Логика взаимодействия для PlayPanelControl.xaml
+    /// </summary>
+    public partial class PlayPanelControl : UserControl
+    {
+        public PlayPanelControl()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty AlbumIconResourceProperty = DependencyProperty.Register(
+            "AlbumIconResource", typeof (string), typeof (PlayPanelControl), new PropertyMetadata(default(string)));
+
+        public string AlbumIconResource
+        {
+            get { return (string) GetValue(AlbumIconResourceProperty); }
+            set { SetValue(AlbumIconResourceProperty, value); }
+        }
+        public bool IsPlaying { get; set; }
+    }
+    
+}
