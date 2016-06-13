899d5a6abea399c7a438953346b213b569d10b0b
pu1s <pu1s@outlook.com>
2016-06-13 12:35:02 +0300

    + curPos

--- FreeAmp/Core/Playlist.cs
+++ FreeAmp/Core/Playlist.cs
@@ -11,9 +11,20 @@ namespace FreeAmp.Core
             EmptyTrackList();
         }
 
+        private uint curPos;
         public string Name { get; set; }
         public List<Track> Tracks { get; }
         public int Count => Tracks.Count;
+
+        public uint CurPos
+        {
+            get
+            {
+                return Tracks.Count == 0 ? 0 : curPos;
+            }
+            set { curPos = value; }
+        }
+
         public Track this[int index] => Tracks[index];
         private static TrackList EmptyTrackList()
         {
