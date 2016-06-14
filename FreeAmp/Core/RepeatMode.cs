a7e9515918a3799c0b49c89b1fa5f736afb39099
pu1s <pu1s@outlook.com>
2016-06-14 17:39:42 +0300

    <<<>>>

---
+++ FreeAmp/Core/RepeatMode.cs
@@ -0,0 +1,12 @@
+namespace FreeAmp.Core
+{
+    /// <summary>
+    /// Перечисление, тип повтора
+    /// </summary>
+    public enum RepeatMode
+    {
+        None,
+        Track,
+        All
+    };
+}
--- FreeAmp/Core/TrackList.cs
+++ FreeAmp/Core/TrackList.cs
@@ -1,26 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace FreeAmp.Core
 {
+    public abstract class Player
+    {
+    }
+
+    /// <summary>
+    /// Список треков
+    /// </summary>
     public class TrackList
     {
+        /// <summary>
+        /// статическое поле, показывающее заполнение треклиста
+        /// </summary>
         private static bool _isEmpty;
 
+        /// <summary>
+        /// Состояние листа (пустой лист)
+        /// </summary>
         public static readonly TrackList Empty = EmptyTrackList();
 
-        private uint curPos;
+        /// <summary>
+        /// указатель на текущий трек
+        /// </summary>
+        private uint _curPos;
 
+        /// <summary>
+        /// Статический конструктор
+        /// </summary>
         static TrackList()
         {
             _isEmpty = true;
         }
 
+        /// <summary>
+        /// Приватный конструктор
+        /// </summary>
         private TrackList()
         {
             Tracks = new List<Track>();
         }
 
+        /// <summary>
+        /// Конструктор с указанием имени трек листа
+        /// </summary>
+        /// <param name="name">
+        /// Имя
+        /// </param>
+        public TrackList(string name) : this()
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        /// Возвращает состояние заполненности трек листа
+        /// </summary>
         public bool IsEmpty
         {
             get
@@ -31,22 +68,43 @@ namespace FreeAmp.Core
             private set { _isEmpty = value; }
         }
 
+        /// <summary>
+        /// Возвращает и присваивает имя трек листа
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Список треков
+        /// </summary>
         public List<Track> Tracks { get; }
 
+        /// <summary>
+        /// Количество элементов списка треков
+        /// </summary>
         public int Count => Tracks.Count;
 
+        /// <summary>
+        /// Возвращает указатель на текущий трек
+        /// </summary>
         public uint CurPos
         {
-            get { return Tracks.Count == 0 ? 0 : curPos; }
+            get { return Tracks.Count == 0 ? 0 : _curPos; }
             set
             {
                 if (value >= Tracks.Count) throw new ArgumentOutOfRangeException();
-                curPos = value;
+                _curPos = value;
             }
         }
 
+        /// <summary>
+        /// Перечислитель коллекции
+        /// </summary>
+        /// <param name="index">
+        /// Индекс
+        /// </param>
+        /// <returns>
+        /// Трек
+        /// </returns>
         public Track this[int index]
         {
             get
@@ -59,22 +117,43 @@ namespace FreeAmp.Core
             }
         }
 
+        /// <summary>
+        /// Статическая функция, реализующая "обнуление списка"
+        /// </summary>
+        /// <returns>
+        /// <see cref="TrackList"/>
+        /// </returns>
         private static TrackList EmptyTrackList()
         {
             var tr = new TrackList {IsEmpty = _isEmpty = true};
             return tr;
         }
 
+        /// <summary>
+        /// Добавляет трек <see cref="Track"/>в конец списка
+        /// </summary>
+        /// <param name="track">
+        /// Трек
+        /// </param>
         public void AppendTrack(Track track)
         {
             Tracks.Add(track);
         }
 
+        /// <summary>
+        /// Добавляет массив треков <see cref="Track"/>в конец списка
+        /// </summary>
+        /// <param name="tracks">
+        /// Массив треков <see cref="Track"/>
+        /// </param>
         public void AppendTracks(Track[] tracks)
         {
             Tracks.AddRange(tracks);
         }
 
+        /// <summary>
+        /// Перемещает указатель трека <see cref="Track"/> на одну позицию вперед
+        /// </summary>
         public void NextTrack()
         {
             if (IsEmpty)
@@ -87,9 +166,12 @@ namespace FreeAmp.Core
                 throw new TrackListException("Список воспроизведения достиг конечной точки!",
                     new Dictionary<int, string>(1) {{1, Tracks.Count.ToString()}});
             }
-            curPos++;
+            _curPos++;
         }
 
+        /// <summary>
+        /// Перемещает указатель трека <see cref="Track"/> на одну позицию назад
+        /// </summary>
         public void PreviewTrack()
         {
             if (IsEmpty)
@@ -102,9 +184,18 @@ namespace FreeAmp.Core
                 throw new TrackListException("Список воспроизведения достиг конечной точки!",
                     new Dictionary<int, string>(1) {{1, Tracks.Count.ToString()}});
             }
-            curPos--;
+            _curPos--;
         }
 
+        /// <summary>
+        /// Добаляет трек в указанное место в списке <see cref="TrackList"/>
+        /// </summary>
+        /// <param name="track">
+        /// Трек <see cref="Track"/>
+        /// </param>
+        /// <param name="pos">
+        /// Позиция, в которую осуществляется добавление
+        /// </param>
         public void InsertTrack(Track track, int pos)
         {
             if (pos < 0 && pos > Tracks.Count)
@@ -114,6 +205,15 @@ namespace FreeAmp.Core
             Tracks.Insert(pos, track);
         }
 
+        /// <summary>
+        /// Добаляет треки в указанное место в списке <see cref="TrackList"/>
+        /// </summary>
+        /// <param name="tracks">
+        /// Массив треков <see cref="Track"/>
+        /// </param>
+        /// <param name="pos">
+        /// Позиция, в которую осуществляется добавление
+        /// </param>
         public void InsertTracks(Track[] tracks, int pos)
         {
             if (pos < 0 && pos > Tracks.Count)
@@ -123,6 +223,15 @@ namespace FreeAmp.Core
             Tracks.InsertRange(pos, tracks);
         }
 
+        /// <summary>
+        /// Перемещает трек из одной позиции на другую
+        /// </summary>
+        /// <param name="beforepos">
+        /// Нальная позиция
+        /// </param>
+        /// <param name="lastpos">
+        /// Конечная позиция
+        /// </param>
         public void MovieTrack(int beforepos, int lastpos)
         {
             if (IsEmpty)
@@ -144,6 +253,128 @@ namespace FreeAmp.Core
             Tracks.RemoveAt(beforepos);
             //TODO реализация метода удаления не выполнена
             InsertTrack(track, lastpos);
+        }
+
+        /// <summary>
+        /// Возвращает текущий трек <see cref="Track"/> из списка <see cref="Tracks"/>
+        /// </summary>
+        /// <returns>
+        /// Трек <see cref="Track"/>
+        /// </returns>
+        public Track GetCurrentTrack()
+        {
+            if (IsEmpty)
+            {
+                throw new TrackListException("Список воспроизведения пуст!",
+                    new Dictionary<int, string>(1) {{1, Tracks.Count.ToString()}});
+            }
+            return Tracks[(int) _curPos];
+        }
+
+        /// <summary>
+        /// Возвращает следующий трек <see cref="Track"/> из списка <see cref="Tracks"/>
+        /// </summary>
+        /// <returns>
+        /// Трек <see cref="Track"/>
+        /// </returns>
+        public Track GetPreviewTrack()
+        {
+            if (IsEmpty)
+            {
+                throw new TrackListException("Список воспроизведения пуст!",
+                    new Dictionary<int, string>(1) {{1, Tracks.Count.ToString()}});
+            }
+            return _curPos == 0 ? null : Tracks[(int) _curPos - 1];
+        }
+
+        /// <summary>
+        /// Возвращает следующий трек <see cref="Track"/> из списка <see cref="Tracks"/>
+        /// </summary>
+        /// <returns>
+        /// Трек <see cref="Track"/>
+        /// </returns>
+        public Track GetNextTrack()
+        {
+            if (IsEmpty)
+            {
+                throw new TrackListException("Список воспроизведения пуст!",
+                    new Dictionary<int, string>(1) {{1, Tracks.Count.ToString()}});
+            }
+            if (_curPos == 0 && _curPos < Tracks.Count)
+            {
+                return Tracks[(int) _curPos + 1];
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Очищает текущий трек лист <see cref="TrackList"/>
+        /// </summary>
+        public void ClearTrackList()
+        {
+            Tracks.Clear();
+        }
+
+        /// <summary>
+        /// Удаляет трек <see cref="Track"/> из треклиста <see cref="Tracks"/>
+        /// </summary>
+        /// <param name="track">
+        /// Трек <see cref="Track"/>
+        /// </param>
+        public void RemoveTrack(Track track)
+        {
+            if (IsEmpty)
+            {
+                throw new TrackListException("Список воспроизведения пуст!",
+                    new Dictionary<int, string>(1) {{1, Tracks.Count.ToString()}});
+            }
+            if (Tracks.Contains(track))
+            {
+                Tracks.Remove(track);
+            }
+        }
+
+        /// <summary>
+        /// Удаляет трек <see cref="Track"/> из треклиста <see cref="Tracks"/> по указанному индексу
+        /// </summary>
+        /// <param name="index">
+        /// Индекс
+        /// </param>
+        public void RemoveTrackAt(int index)
+        {
+            if (IsEmpty)
+            {
+                throw new TrackListException("Список воспроизведения пуст!",
+                    new Dictionary<int, string>(1) {{1, Tracks.Count.ToString()}});
+            }
+            if (index <= Tracks.Count)
+            {
+                Tracks.RemoveAt(index);
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает случайный выбор трека
+        /// </summary>
+        public bool IsShuffle { get; set; }
+
+        /// <summary>
+        /// Возвращает и задает алгоритм повтора треков
+        /// </summary>
+        public RepeatMode IsRepeat { get; set; }
+
+        /// <summary>
+        /// Вычисляет псевдослучайное число
+        /// </summary>
+        private void Shuffle()
+        {
+            if (IsEmpty)
+            {
+                throw new TrackListException("Список воспроизведения пуст!",
+                    new Dictionary<int, string>(1) {{1, Tracks.Count.ToString()}});
+            }
+            var rnd = new Random(Count);
+            _curPos = (uint) rnd.Next(0, Count);
         }
     }
 }
