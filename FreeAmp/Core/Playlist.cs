b586aa7d82a01ac32645294e4f8c5d991430106f
pu1s <pu1s@outlook.com>
2016-06-14 08:09:06 +0300

    + lic

--- FreeAmp/Core/Playlist.cs
+++ FreeAmp/Core/Playlist.cs
@@ -1,31 +1,28 @@
 ﻿using System.Collections.Generic;
-using System.Dynamic;
-using System.Windows;
 
 namespace FreeAmp.Core
 {
     public class TrackList
     {
+        private uint curPos;
+
         static TrackList()
         {
             EmptyTrackList();
         }
 
-        private uint curPos;
         public string Name { get; set; }
         public List<Track> Tracks { get; }
         public int Count => Tracks.Count;
 
         public uint CurPos
         {
-            get
-            {
-                return Tracks.Count == 0 ? 0 : curPos;
-            }
+            get { return Tracks.Count == 0 ? 0 : curPos; }
             set { curPos = value; }
         }
 
         public Track this[int index] => Tracks[index];
+
         private static TrackList EmptyTrackList()
         {
             return new TrackList();
