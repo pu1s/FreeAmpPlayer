40759fc65dad78bdc5a2f9ed1d2a82fee886371d
pu1s <pu1s@outlook.com>
2016-06-30 21:02:50 +0300

    444

--- FreeAmp/MainWindow.xaml.cs
+++ FreeAmp/MainWindow.xaml.cs
@@ -1,73 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Management;
-using System.Reflection.Emit;
-using System.Timers;
 using System.Windows;
-using System.Windows.Data;
-
 using System.Windows.Input;
 using System.Windows.Threading;
+using freeampcorelib;
 using FreeAmp.Core;
 using Microsoft.Win32;
-using NAudio.CoreAudioApi;
-using NAudio.Utils;
-using NAudio.Wave;
-using Un4seen.Bass;
-using freeampcorelib;
-using TrackList = freeampcorelib.TrackList;
-
+using Track = freeampcorelib.Track;
 
 namespace FreeAmp
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    ///     Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-
-
-        public SoundPlayer sp { get; set; } = null;
-        private freeampcorelib.TrackList tl = new TrackList();
         private DispatcherTimer timer;
-        
+        private readonly TrackList tl = new TrackList();
+
         public MainWindow()
         {
             InitializeComponent();
 
             #region Command implementation
 
-            this.CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand, this.OnCloseWindow));
-            this.CommandBindings.Add(new CommandBinding(SystemCommands.MaximizeWindowCommand, this.OnMaximizeWindow,
-                this.OnCanResizeWindow));
-            this.CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand, this.OnMinimizeWindow,
-                this.OnCanMinimizeWindow));
-            this.CommandBindings.Add(new CommandBinding(SystemCommands.RestoreWindowCommand, this.OnRestoreWindow,
-                this.OnCanResizeWindow));
-            this.CommandBindings.Add(new CommandBinding(SystemCommands.ShowSystemMenuCommand, this.OnSystemMenuShow,
+            CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand, OnCloseWindow));
+            CommandBindings.Add(new CommandBinding(SystemCommands.MaximizeWindowCommand, OnMaximizeWindow,
+                OnCanResizeWindow));
+            CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand, OnMinimizeWindow,
+                OnCanMinimizeWindow));
+            CommandBindings.Add(new CommandBinding(SystemCommands.RestoreWindowCommand, OnRestoreWindow,
+                OnCanResizeWindow));
+            CommandBindings.Add(new CommandBinding(SystemCommands.ShowSystemMenuCommand, OnSystemMenuShow,
                 null));
 
             #endregion
 
-            
             sp = new SoundPlayer();
             sp.TrackLoaded += Sp_TrackLoaded;
             timer = new DispatcherTimer {Interval = TimeSpan.FromMilliseconds(1000)};
             //timer.Tick += Timer_Tick;
             //_trackList.AppendTrack(
             //    new Track(@"E:\23_justin_timberlake_cant_stop_the_feeling_myzuka.fm.mp3"));
-           
         }
 
-       
+
+        public SoundPlayer sp { get; set; }
+
 
         private void Sp_TrackLoaded(object sender, EventArgs e)
         {
             slider.Maximum = sp.TrackTotalTime;
-          
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -75,10 +57,35 @@ namespace FreeAmp
             slider.Value = sp.CurrentTrackTime;
         }
 
-       
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Загрузите данные, установив свойство CollectionViewSource.Source:
+            // trackListViewSource.Source = [универсальный источник данных]
+        }
+
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //sp.Load(_trackList.GetCurrentTrack());
+            //if(!timer.IsEnabled) timer.Start();
+            //sp.Play();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            ofd.ShowDialog(this);
+            tl.Items.Add(new Track(ofd.FileName));
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            sp.CurrentTrackTime = slider.Value;
+        }
 
         #region CommandWindow
-
 
         private void OnSystemMenuShow(object target, ExecutedRoutedEventArgs e)
         {
@@ -87,12 +94,12 @@ namespace FreeAmp
 
         private void OnCanResizeWindow(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = this.ResizeMode == ResizeMode.CanResize || this.ResizeMode == ResizeMode.CanResizeWithGrip;
+            e.CanExecute = ResizeMode == ResizeMode.CanResize || ResizeMode == ResizeMode.CanResizeWithGrip;
         }
 
         private void OnCanMinimizeWindow(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = this.ResizeMode != ResizeMode.NoResize;
+            e.CanExecute = ResizeMode != ResizeMode.NoResize;
         }
 
         private void OnCloseWindow(object target, ExecutedRoutedEventArgs e)
@@ -115,43 +122,6 @@ namespace FreeAmp
             SystemCommands.RestoreWindow(this);
         }
 
-
         #endregion
-
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            // Загрузите данные, установив свойство CollectionViewSource.Source:
-            // trackListViewSource.Source = [универсальный источник данных]
-        }
-
-
-
-
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-           
-           
-            //sp.Load(_trackList.GetCurrentTrack());
-            //if(!timer.IsEnabled) timer.Start();
-            //sp.Play();
-           
-        }
-
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-             
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Multiselect = true;
-            ofd.ShowDialog(this);
-            tl.Items.Add(new freeampcorelib.Track(ofd.FileName));
-           
-
-        }
-
-        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            sp.CurrentTrackTime = slider.Value;
-        }
     }
 }
