3b5e8163aea8fc243ec1d3bd05327e5297e2affc
pu1s <pu1s@outlook.com>
2016-07-05 21:31:56 +0300

    -

--- freeampcontrols/freeampTrackBarControl.xaml.cs
+++ freeampcontrols/freeampTrackBarControl.xaml.cs
@@ -24,5 +24,24 @@ namespace freeampcontrols
         {
             InitializeComponent();
         }
+
+
+
+
+        public int MyProperty
+        {
+            get { return (int)GetValue(MyPropertyProperty); }
+            set {
+                SetValue(MyPropertyProperty, value);
+                }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPropertyProperty =
+            DependencyProperty.Register("MyProperty", typeof(int), typeof(freeampTrackBarControl), new PropertyMetadata(0));
+
+
+
+
     }
 }
