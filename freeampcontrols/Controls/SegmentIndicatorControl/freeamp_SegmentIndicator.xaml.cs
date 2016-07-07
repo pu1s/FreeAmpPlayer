f1dfcc8712af8614ff7c4662c0f8f6df711b0368
pu1s <pu1s@outlook.com>
2016-07-07 12:46:42 +0300

    +++++

---
+++ freeampcontrols/Controls/SegmentIndicatorControl/freeamp_SegmentIndicator.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace freeampcontrols.Controls.SegmentIndicatorControl
+{
+    /// <summary>
+    /// Логика взаимодействия для freeamp_SegmentIndicator.xaml
+    /// </summary>
+    public partial class freeamp_SegmentIndicator : UserControl
+    {
+        public freeamp_SegmentIndicator()
+        {
+            InitializeComponent();
+        }
+    }
+}
