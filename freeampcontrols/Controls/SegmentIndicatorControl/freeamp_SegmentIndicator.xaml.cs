18920fe7054a3f22b4a69258f8a2786b07ed393d
pu1s <pu1s@outlook.com>
2016-07-07 14:53:01 +0300

    (-;

--- freeampcontrols/Controls/SegmentIndicatorControl/freeamp_SegmentIndicator.xaml.cs
+++ freeampcontrols/Controls/SegmentIndicatorControl/freeamp_SegmentIndicator.xaml.cs
@@ -1,46 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace freeampcontrols.Controls.SegmentIndicatorControl
 {
     /// <summary>
-    /// Логика взаимодействия для freeamp_SegmentIndicator.xaml
+    ///     Логика взаимодействия для freeamp_SegmentIndicator.xaml
     /// </summary>
     public partial class freeamp_SegmentIndicator : UserControl
     {
+        public static readonly DependencyProperty SignalProperty = DependencyProperty.Register(
+            "Signal", typeof (bool), typeof (freeamp_SegmentIndicator), new PropertyMetadata(OnSignalChange));
+
+        public static readonly DependencyProperty SegmentStateProperty = DependencyProperty.Register(
+            "SegmentState", typeof (bool), typeof (freeamp_SegmentIndicator),
+            new PropertyMetadata(OnSegmentStateChanged));
+
         public freeamp_SegmentIndicator()
         {
             InitializeComponent();
         }
-        public static readonly DependencyProperty SignalProperty = DependencyProperty.Register(
-            "Signal", typeof(bool), typeof(freeamp_SegmentIndicator), new PropertyMetadata(OnSignalChange));
-
-        public static readonly DependencyProperty SegmentStateProperty = DependencyProperty.Register(
-            "SegmentState", typeof(bool), typeof(freeamp_SegmentIndicator),
-            new PropertyMetadata(OnSegmentStateChanged));
 
 
         public bool SegmentState
         {
-            get { return (bool)GetValue(SegmentStateProperty); }
+            get { return (bool) GetValue(SegmentStateProperty); }
             set { SetValue(SegmentStateProperty, value); }
         }
 
         public bool Signal
         {
-            get { return (bool)GetValue(SignalProperty); }
+            get { return (bool) GetValue(SignalProperty); }
             set { SetValue(SignalProperty, value); }
         }
 
@@ -49,13 +38,13 @@ namespace freeampcontrols.Controls.SegmentIndicatorControl
 
         private static void OnSignalChange(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
         {
-            ((freeamp_SegmentIndicator)dependencyObject).IsSignal = (bool)args.NewValue;
+            ((freeamp_SegmentIndicator) dependencyObject).IsSignal = (bool) args.NewValue;
         }
 
         private static void OnSegmentStateChanged(DependencyObject dependencyObject,
             DependencyPropertyChangedEventArgs args)
         {
-            ((freeamp_SegmentIndicator)dependencyObject).SegmentState = (bool)args.NewValue;
+            ((freeamp_SegmentIndicator) dependencyObject).SegmentState = (bool) args.NewValue;
         }
     }
 }
